edd101e519f5d53a268cb4ecccfe5294f967ab83
Michael Arntzenius <daekharel@gmail.com>
2015-09-26 01:15:24 +0100

    initial import

---
+++ sessiontypes.rkt
@@ -0,0 +1,133 @@
+#lang racket
+
+(require "util.rkt")
+
+;; ---------- GRAMMAR ----------
+
+;; tags N                   are symbols
+;; channel variables C, D   are symbols
+;; variables X, Y           are symbols
+;;
+;; expressions E            are racket expressions
+
+;; process expressions are lists of process statements.
+;;
+;;   P  ::= (S ...)
+
+;; process statements are complicated:
+;;  S   ::= (spawn C <- P)          -- spawning a process (cut)
+;;          (forward C to D)        -- forwarding a channel (identity)
+;;
+;;          -- tensor and -o ("lolli") rules for channels
+;;          (C <- chan D)           -- sends C to D
+;;          (chan D <- C)           -- receives D from C
+;;          -- tensor and -o ("lolli") for values
+;;          (C <- val E)            -- sending a value
+;;          (val X <- C)            -- receiving a value
+;;
+;;          -- & ("with") and ⊕ ("oplus") rules.
+;;          (C <- tag N)            -- sends tag N to C
+;;          -- receives a tag on C then dispatches to the matching case
+;;          (case C (if N then . P) ...)
+;;
+;;          -- unit rules
+;;          (wait C)                -- waits on another channel to close
+;;          (close C)               -- closes the channel we own, C
+;;
+;; NB. A process expression body must be terminated by either (close) or a
+;; case-statement. Moreover, case- & close-statements may *only* appear in
+;; terminal position. The typechecker enforces this.
+
+;; types are like so (yes, the dot notation means dotted lists!):
+;;  A,B ::= (send A . B)                   -- tensor, ⊗
+;;          (recv A . B)                   -- lolli,  -o
+;;          (send value . A)               -- tensor, ⊗  (but for racket values)
+;;          (recv value . A)               -- lolli,  -o (but for racket values)
+;;          (recv one of (N . A) ...)      -- with, &
+;;          (send one of (N . A) ...)      -- sum,  ⊕
+;;           -- NB. in with and sum, all tags must be distinct.
+;;          ()                             -- unit, 1, meaning "done"
+;;
+;; for example, the type
+;; >    (recv value send value)
+;; describes a process which receives a value, then sends one, then is done!
+
+;; how am I gonna typecheck this?
+;; bidirectionally. with subtyping for cases.
+
+
+;;; ---------- TYPE OPERATIONS & TYPE CHECKING ----------
+;;;
+;;; You can skip reading this unless you care about type inference details.
+
+(define (type-error-lub a b)
+  (error (format "type error: could not unify ~a with ~a" a b)))
+
+;; this is a neat hack, but can produce the confusing error message:
+;;  "type error: could not unify FOO with FOO"
+(define (type-ok x) (type-lub x x))
+
+;; least upper bound under subtyping. "unions" or "unifies" two types to find
+;; the smallest one that includes both.
+(define (type-lub a b)
+  (match* (a b)
+    [('() '()) '()]
+    [(`(recv . ,as) `(recv . ,bs)) `(recv . ,(type-lub-rest 'recv as bs))]
+    [(`(send . ,as) `(send . ,bs)) `(send . ,(type-lub-rest 'send as bs))]
+    [(_ _) (type-error-lub a b)]))
+
+(define (type-lub-rest send-or-recv a b)
+  (define (err) (type-error-lub (cons send-or-recv a) (cons send-or-recv b)))
+  (match* (a b)
+    [(`(one of . ,as) `(one of . ,bs)) ;; the interesting case
+      ;; sorry, this is pretty inscrutable
+      (define a-map (make-immutable-hash as))
+      (define b-map (make-immutable-hash bs))
+      ;; ⊕(x: A) ≤ ⊕(x: A, y: B)
+      ;; &(x: A, y: B) ≤ &(x: A)
+      (define keys
+        ((match send-or-recv ['send set-union] ['recv set-intersect])
+          (list->set (map car as))
+          (list->set (map car bs))))
+      `(one of
+         ,@(for/list ([k keys])
+             (cons k
+               ;; if A ≤ B then ⊕(x: A) ≤ ⊕(x: B)
+               ;; if A ≤ B then &(x: A) ≤ &(x: B)
+               (match* ((hash-ref a-map k #f) (hash-ref b-map k #f))
+                 [(#f x) (type-ok x)]
+                 [(x #f) (type-ok x)]
+                 [(x y) (type-lub x y)]))))]
+    [(`(value . ,as) `(value . ,bs)) `(value . ,(type-lub as bs))]
+    ;; error cases
+    [(`(value . ,_) _) (err)]
+    [(_ `(value . ,_)) (err)]
+    [(`(one of . ,as) _) (err)]
+    [(_ `(one of . ,as)) (err)]
+    ;; has to go last b/c it has no keywords
+    [((cons a as) (cons b bs)) (cons (type-lub a b) (type-lub as bs))]))
+
+
+;; ---------- STATE STRUCTURES ----------
+;; A world-state is a hash from channel IDs to channel info structures.
+(struct world-state (channel-map) #:transparent)
+
+;; A channel info structure has three fields:
+;;
+;; proc: The state-structure for the process owning the channel.
+;;
+;; readers: List of processes waiting to input on the channel, identified by the
+;; channel they own (so that we can look them up in the channel-map).
+;;
+;; writers: Likewise, a list of processes waiting to output on the channel.
+;;
+;; It is an error for either `readers' or `writers' to have >1 member; the type
+;; system guarantees this will not happen. In other systems this might cause
+;; nondeterministic behavior (a "race").
+(struct channel-info (proc readers writers) #:transparent)
+
+;; A process state structure has two fields:
+;;
+;; env: Hash mapping (either regular or channel) variables to their values.
+;; expr: The current process expression.
+(struct process-state (env expr) #:transparent)
---
+++ util.rkt
@@ -0,0 +1,91 @@
+#lang racket
+
+(require racket (for-syntax syntax/parse))
+
+(provide
+  define-syntax-parser TODO fn enum enum-case
+  (for-syntax syntax-parse syntax-parser)  ;; re-export
+  assert! warn! flip print-error index-of eqmap stream-take
+  hash-union-with hash-intersection-with)
+
+(define-syntax define-syntax-parser
+  (syntax-parser
+    [(_ name:id (pattern body ...) ...)
+      #'(define-syntax name
+          (syntax-parser
+            (pattern body ...)
+            ...))]
+    [(_ (name:id pattern ...) body ...)
+      #'(define-syntax-parser name
+          [(_ pattern ...) body ...])]))
+
+(define-syntax-parser TODO
+  [_:id #'(error "TODO: unimplemented")])
+
+(begin-for-syntax
+  (define-syntax-class fn-clause
+    (pattern ((param ...) body ...)
+      #:attr pattern #'(list param ...))
+    (pattern ((param ... . rest-param:id) body ...)
+      #:attr pattern #'(list-rest param ... rest-param))))
+
+(define-syntax-parser fn
+  [(_ (name params ...) body ...)
+    #'(define (name params ...) body ...)]
+  [(_ name:id c:fn-clause ...)
+    #'(define/match (name . _)
+        [(c.pattern) c.body ...]
+        ...)])
+
+(define-syntax-rule (enum-case enum-name (branch-name args ...))
+  (struct branch-name enum-name (args ...) #:transparent))
+
+(define-syntax-rule (enum name branch ...)
+  (begin
+    (struct name () #:transparent)
+    (enum-case name branch) ...))
+
+
+;;; Miscellaneous utilities
+(define (assert! t) (unless t (error "ASSERTION FAILURE")))
+(define (warn! msg) (displayln (format "WARNING: ~a" msg)) )
+
+(define ((flip f) x y) (f y x))         ;do we need this?
+
+(define (print-error err)
+  (printf "error: ~a\n" (exn-message err)))
+
+(define (index-of v lst)
+  (let loop ([i 0] [l lst])
+    (match l
+      ['() #f]
+      [(cons x xs) (if (equal? x v) i (loop (+ 1 i) xs))])))
+
+(define (eqmap eq l . lsts)
+  (define len (length l))
+  (and (andmap (lambda (l) (= len (length l))) lsts)
+       (apply andmap eq l lsts)))
+
+(define (stream-take n s)
+  (for/list ([x (in-stream s)]
+             [_ (in-range n)])
+    x))
+
+(define (stream-append-lazy stream stream-thunk)
+  (if (stream-empty? stream) (stream-thunk)
+    (stream-cons (stream-first stream)
+      (stream-append-lazy (stream-rest stream stream-thunk)))))
+
+(define (hash-union-with a b f)
+  (define keys (set-union (list->set (dict-keys a)) (list->set (dict-keys b))))
+  (for/hash ([k keys])
+    (if (not (dict-has-key? a k))
+      (dict-ref b k)
+      (if (not (dict-has-key? b k))
+        (dict-ref a k)
+        (f (dict-ref a k) (dict-ref b k))))))
+
+(define (hash-intersection-with a b f)
+  (for/hash ([k (in-dict-keys a)]
+              #:when (dict-has-key? b k))
+    (f (dict-ref a k) (dict-ref b k))))
